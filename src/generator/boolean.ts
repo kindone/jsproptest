8e3e52f12652a9573a100fd90a1550ea8c7967b3
Won-Wook Hong <jradoo@gmail.com>
2021-01-04 22:02:41 +0900

    boolean with trueProb, char generators

--- src/generator/boolean.ts
+++ src/generator/boolean.ts
@@ -2,9 +2,9 @@ import { Arbitrary, Generator } from '../Generator';
 import { Random } from '../Random';
 import { shrinkableBoolean } from '../shrinker/boolean';
 
-export function booleanGen(): Generator<boolean> {
+export function booleanGen(trueProb = 0.5): Generator<boolean> {
     return new Arbitrary((random: Random) => {
-        const value = random.nextBoolean();
+        const value = random.nextBoolean(trueProb);
         return shrinkableBoolean(value);
     });
 }
